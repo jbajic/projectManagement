139496906d40e3d27a0ee30805cff8f433e72671
jbajic <jure.bajic94@gmail.com>
2016-06-20 10:33:34 +0200

    test001

--- public/js/setup.js
+++
@@ -1,8 +0,0 @@
-// location.protocol gives http or https
-// location.hostname gives hostname
-var myUrl = location.protocol + "//" + location.hostname + "/";
-
-// ajax setup for csrf tokens
-$.ajaxSetup({
-   headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }
-});
